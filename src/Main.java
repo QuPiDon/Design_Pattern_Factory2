fc538af55d5c60324a20aa92f7d92852df9ec7da
EVGENY KVEST <evgenykvest@gmail.com>
2025-01-08 10:06:20 +0100

    Die Aufgabe 2_2 optimiert. Die Prüfung der Datentypen hinzugefügt.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 public class Main {
 
@@ -9,15 +10,20 @@ public class Main {
             List<String> arrayList = ListFactory.create("A");
             arrayList.add("Element_1");
             arrayList.add("Element_2");
-            System.out.println("ArrayList: " + arrayList);
+
+            if(arrayList instanceof ArrayList) {System.out.println("ArrayList: " + arrayList);}
 
             List<String> vectorList = ListFactory.create("V");
             vectorList.add("Element_1");
             vectorList.add("Element_2");
             System.out.println("VectorList: " + vectorList);
+
+            if(vectorList instanceof ArrayList) {System.out.println("Vector: " + vectorList);}
         } catch (IllegalArgumentException e) {
             System.out.println("Fehler: + e.getMessage");
         }
+
+
 
 
 
